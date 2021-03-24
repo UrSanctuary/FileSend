4508365d2eb6aa944b5cd92d6dedc3e740a27e27
UrSanctuary <64437373+UrSanctuary@users.noreply.github.com>
2021-03-24 21:00:17 +0200

    Update comb2.cpp

--- comb2.cpp
+++ comb2.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <vector>
-
+#include <time.h>
 int main()
 {
     std::vector<int> sexOfShip;
     int i = 0;
     int b = 0;
+    srand(time(NULL));
     //girls = i-1; 
     //boy = 1;
-    while(b != 1)
+    while (b != 1)
     {
         b = rand() % 2;
         sexOfShip.push_back(b);
-        std::cout<<sexOfShip[i]<<std::endl;
+        std::cout << sexOfShip[i] << std::endl;
         i++;
     }
-    int pers = ((i-1)/i)*100;
-    std::cout<<"Girls in this family "<< pers<< "\nBoys in this family "<<100-pers<<std::endl;
+    double pers = (((double)i - 1) / (double)i)*100 ;
+    std::cout << "Girls in this family " << pers << "%\nBoys in this family " << (100 - pers) << "%"<<std::endl;
 }
-// 0 1 - 50/50
-// 0 0 1 75/25
-// ... 
+
