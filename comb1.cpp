c250b2c18a96d01cb3f52bd992e92a7a023af4b8
UrSanctuary <64437373+UrSanctuary@users.noreply.github.com>
2021-03-24 20:33:34 +0200

    Update comb1.cpp

--- comb1.cpp
+++ comb1.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
-#include <vector>
+#include <time.h>
 
 int main()
 {
-    std::vector<int> binVector;
+   
     int count = 0;
     int i = 0;
-    int num = 0;
-    while(count < 11)
-    {
-        int b = rand() % 2;
-        binVector.push_back(b);
-        std::cout << binVector[i] << std::endl;
-        if((binVector[i] == 1 && binVector[i+1] == 1 )|| (binVector[i] == 0 && binVector[i+1] == 0))
+    int b;
+    srand(time(NULL));
+    while (count < 11)
+    {   
+        b = rand() % 2;
+        if (b=rand() % 2 == b)
         {
             count++;
         }
-        else{
+        else {
             count = 0;
         }
-        num = binVector[i];
+        std::cout << b << std::endl;
         i++;
     }
-    std::cout<<"After "<< i << " times of ___ you will get 11 times of "<< num << std::endl;
+    std::cout << "After " << i << " times of you will get 11 times of " << b << std::endl;
 }
