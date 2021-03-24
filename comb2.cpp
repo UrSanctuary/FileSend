4bd31a209d1b9ab216df6b90b211f962412cd592
UrSanctuary <64437373+UrSanctuary@users.noreply.github.com>
2021-03-24 17:52:34 +0200

    Update comb2.cpp
    bag fixed:
    1)rund() 1 or 0

--- comb2.cpp
+++ comb2.cpp
@@ -5,16 +5,18 @@ int main()
 {
     std::vector<int> sexOfShip;
     int i = 0;
+    int b = 0;
     //girls = i-1; 
     //boy = 1;
-    while(sexOfShip[i]!=1)
+    while(b != 1)
     {
-        srand(time(NULL));
-        sexOfShip[i] = rand()%1;
+        b = rand() % 2;
+        sexOfShip.push_back(b);
+        std::cout<<sexOfShip[i]<<std::endl;
         i++;
     }
     int pers = ((i-1)/i)*100;
-    std::cout<<"Girls in this family "<< rers<< "/nBoys in this family "<<100-pers<<std::endl;
+    std::cout<<"Girls in this family "<< pers<< "\nBoys in this family "<<100-pers<<std::endl;
 }
 // 0 1 - 50/50
 // 0 0 1 75/25
