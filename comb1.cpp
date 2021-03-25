7c4bc39107e0b401e581533ca84875edff7ce1fc
UrSanctuary <64437373+UrSanctuary@users.noreply.github.com>
2021-03-25 17:31:18 +0200

    Update comb1.cpp

--- comb1.cpp
+++ comb1.cpp
@@ -5,21 +5,15 @@ int main()
 {
     int count = 0;
     int i = 0;
-    int a,b;
+    int a, b;
     srand((unsigned)time(NULL));
     a = rand() % 2;
     while (count < 11)
     {
         b = rand() % 2;
-        std::cout << i << ": " << b << std::endl;
         if (a == b)
         {
             count++;
-            if (count == 11)
-            {
-                break;
-            }
-           
         }
         else {
             count = 0;
@@ -27,5 +21,5 @@ int main()
         a = b;
         i++;
     }
-    std::cout << "\nAfter " << i << " times you will get 11 times of " << b << std::endl;
+    std::cout << "After " << i << " times you will get 11 times of " << b << std::endl;
 }
