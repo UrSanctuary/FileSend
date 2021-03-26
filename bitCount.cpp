ba3ef52c9f77a31450a3d813a31c57752594b04a
UrSanctuary <64437373+UrSanctuary@users.noreply.github.com>
2021-03-26 12:53:50 +0200

    Create bitCount.cpp

---
+++ bitCount.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int countSetBits(int n)
+{
+    unsigned int count = 0;
+    while (n) {
+        count += n & 1;
+        n >>= 1;
+    }
+    return count;
+}
+
+int main()
+{
+    int number;
+    std::cin>>number;
+    std::cout << countSetBits(number);
+    return 0;
+}
+ 
