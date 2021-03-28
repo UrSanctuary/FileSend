68ed4fc14782dc06f783de0956f4c135adcb1604
UrSanctuary <64437373+UrSanctuary@users.noreply.github.com>
2021-03-28 20:26:02 +0300

    Update reverseString.cpp

--- reverseString.cpp
+++ reverseString.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
-//func to split string 
-//func to reverse words in string
-//func to reverse wors/or just use an algorithm
-
+std::vector<std::string> SplitBySpaceWithRev( std::string str)
+{
+    std::vector<std::string> words;
+    std::string word = "";
+    std::string revStr = "";
+    for(int i = 0; i < str.length(); ++i)
+    {
+        if(str[i]==' ')
+        {
+            revStr = std::string(word.rbegin(),word.rend());
+            words.push_back(std::string(revStr));
+            word="";
+            revStr = "";
+        }
+        else
+        {
+            word.push_back(str[i]);
+        }
+        
+    }
+    std::reverse(word.begin(), word.end());
+    words.push_back(word);
+    std::reverse(words.begin(),words.end());
+    return words;
+}
+void PrintVector(std::vector<std::string> v){
+	for(int i=0;i<v.size();++i)
+		std::cout<<v[i]<<" ";
+    std::cout<<"\n";
+}
 int main()
 {
-
+    std::string sentence;
+    std::getline(std::cin, sentence);
+    PrintVector(SplitBySpaceWithRev(sentence));
 }
